a695afe54406ed2a39f7464fdcd48e4da8f582a1
Pedro <101034292+TheWaterBoo@users.noreply.github.com>
2024-08-30 00:21:59 -0600

    Ver 0.4.2.1
    - EncryptNDecrypt method now is working!
    - Key static will be removed later

--- PwdKeychain/Implementations/Blower.cs
+++ PwdKeychain/Implementations/Blower.cs
@@ -15,34 +15,33 @@ namespace PwdKeychain.Implementations
 
         public Blower()
         {
-            _key = Encoding.UTF8.GetBytes("15161718202111");
+            _key = new byte[] {24,87,245,72,44,7,99,112,255,177,101,34,22,58,244,1};
             _padding = new Pkcs7Padding();
         }
 
         public string Encrypter(string pwd)
         {
-            byte[] encryptOutput = CryptNDecrypt(pwd, true);
-
+            byte[] inputBytes = Encoding.UTF8.GetBytes(pwd);
+            byte[] encryptOutput = CryptNDecrypt(inputBytes, true);
             return Convert.ToBase64String(encryptOutput);
         }
 
         public string Decrypter(string zipedPwd)
         {
-            byte[] decryptOutput = CryptNDecrypt(zipedPwd, false);
-
+            byte[] decodedCipherText = Convert.FromBase64String(zipedPwd);
+            byte[] decryptOutput = CryptNDecrypt(decodedCipherText, false);
             return Encoding.UTF8.GetString(decryptOutput);
         }
 
-        private byte[] CryptNDecrypt(string thing, bool forEncrypt)
+        private byte[] CryptNDecrypt(byte[] inputBytes, bool forEncrypt)
         {
-            byte[] inputBytes = Encoding.UTF8.GetBytes(thing);
             BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new BlowfishEngine(), _padding);
             cipher.Init(forEncrypt, new KeyParameter(_key));
             
             byte[] outputBytes = new byte[cipher.GetOutputSize(inputBytes.Length)];
             int length = cipher.ProcessBytes(inputBytes, 0, inputBytes.Length, outputBytes, 0);
             cipher.DoFinal(outputBytes, length);
-
+            
             return outputBytes;
         }
     }
